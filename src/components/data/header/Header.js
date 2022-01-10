f8c7c67f9d7f1124e2abf25bb4fb9dee115907da
nairo-mudumane <nazirmudumane@gmail.com>
2022-01-10 13:31:57 +0200

    updated nav

--- src/components/data/header/Header.js
+++ src/components/data/header/Header.js
@@ -4,9 +4,22 @@ const Header = () => {
   return (
     <header className={styles.header}>
       <div className="container">
-        <div className={`${styles.flexContainer} row`}>
-          <div className={`${styles.logo} col-lg-2`}>logo</div>
-          <nav className={`${styles.logo} col-lg-10`}>navbar</nav>
+        <div className={styles.radius}>
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-12">
+                <nav className={styles.nav}>
+                  <ul>
+                    <li>i</li>
+                    <li>i</li>
+                    <li>i</li>
+                    <li>i</li>
+                  </ul>
+                </nav>
+              </div>
+              <div className={`${styles.border} col-lg-6`}></div>
+            </div>
+          </div>
         </div>
       </div>
     </header>
