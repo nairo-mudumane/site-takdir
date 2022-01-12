15dc28dc7e95b1a32997ebd2ae9ae071226cfd85
nairo-mudumane <nazirmudumane@gmail.com>
2022-01-12 11:01:30 +0200

    created a color pallet

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Header from "./components/data/header/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./global/GlobalColors.css";
 import "./global/GlobalStyle.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
