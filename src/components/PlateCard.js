6086150ff4af982c03aa6842266268d1519f6251
nairo-mudumane <nazirmudumane@gmail.com>
2022-01-12 15:51:01 +0200

    updated food cards

--- src/components/PlateCard.js
+++ src/components/PlateCard.js
@@ -11,6 +11,7 @@ export default function PlateCard({
   desc,
   datePosted,
   rating,
+  bgColor,
 }) {
   function loopStars(rate) {
     console.log(rate);
@@ -27,48 +28,7 @@ export default function PlateCard({
   // big card
   if (big)
     return (
-      <div className={`${styles.card} ${styles.bigCard}`}>
-        <Link to="/">
-          <div className={styles.image}>
-            <img src={image} alt={alt} />
-          </div>
-
-          <div className={styles.title}>
-            <Title text={name} font={1.2} />
-
-            <div className={styles.rating}>
-              <div className={styles.ratingStars}>
-                {rating === 1 ? (
-                  <span className={styles.star}>
-                    <i className="fas fa-star"></i>
-                  </span>
-                ) : (
-                  loopStars(rating)
-                )}
-              </div>
-              {datePosted && (
-                <div className={styles.datePosted}>
-                  <p className="no-margin">em: {datePosted}</p>
-                </div>
-              )}
-            </div>
-          </div>
-
-          {desc && (
-            <div className={styles.desc}>
-              <p className="text-muted no-margin">
-                In sint enim aliqua labore cupidatat Lorem ut velit sit dolore
-                mollit amet.
-              </p>
-            </div>
-          )}
-        </Link>
-      </div>
-    );
-
-  return (
-    <div className={styles.card}>
-      <Link to="/">
+      <Link to="/" className={`${styles.card} ${styles.cardBig}`}>
         <div className={styles.image}>
           <img src={image} alt={alt} />
         </div>
@@ -78,16 +38,58 @@ export default function PlateCard({
 
           <div className={styles.rating}>
             <div className={styles.ratingStars}>
-              <span className={styles.star}>
-                <i className="fas fa-star"></i>
-              </span>
+              {rating === 1 ? (
+                <span className={styles.star}>
+                  <i className="fas fa-star"></i>
+                </span>
+              ) : (
+                loopStars(rating)
+              )}
             </div>
-            <div className={styles.datePosted}>
-              <p className="no-margin">em: 12/12/2012</p>
-            </div>
+            {datePosted && (
+              <div className={styles.datePosted}>
+                <p className="no-margin">em: {datePosted}</p>
+              </div>
+            )}
           </div>
         </div>
+
+        {desc && (
+          <div className={styles.desc}>
+            <p className="text-muted no-margin">
+              In sint enim aliqua labore cupidatat Lorem ut velit sit dolore
+              mollit amet.
+            </p>
+          </div>
+        )}
       </Link>
-    </div>
+    );
+
+  return (
+    <Link
+      to="/"
+      className={`${styles.card} ${styles.cardSmall} ${
+        bgColor === "black" && styles.blackCard
+      } ${bgColor === "red" && styles.redCard}`}
+    >
+      <div className={styles.image}>
+        <img src={image} alt={alt} />
+      </div>
+
+      <div className={styles.title}>
+        <Title text={name} font={1.0} />
+
+        <div className={styles.rating}>
+          <div className={styles.ratingStars}>
+            <span className={styles.star}>
+              <i className="fas fa-star"></i>
+            </span>
+          </div>
+          <div className={styles.datePosted}>
+            <p className="no-margin">em: 12/12/2012</p>
+          </div>
+        </div>
+      </div>
+    </Link>
   );
 }
--- src/pages/home/MainPlates.js
+++ src/pages/home/MainPlates.js
@@ -2,7 +2,9 @@ import React from "react";
 import Container from "../../components/layout/Container";
 import PlateCard from "../../components/PlateCard";
 import styles from "./MainPlates.module.css";
-import imageSrc from "../../assets/food1.png";
+import food1 from "../../assets/food1.png";
+import food2 from "../../assets/food2.png";
+import food3 from "../../assets/food3.png";
 
 export default function MainPlates() {
   return (
@@ -13,9 +15,36 @@ export default function MainPlates() {
           name="Yoshimasa Suchi"
           rating={3}
           datePosted="20/02/2021"
-          image={imageSrc}
+          image={food1}
           alt="Suchi"
           desc="Commodo consequat mollit proident voluptate consequat ipsum quis magna esse elit aliquip sunt amet."
+        />
+
+        <PlateCard
+          name="Suchi 2"
+          bgColor="red"
+          image={food2}
+          alt=""
+          datePosted="04/05/2021"
+          rating={3}
+        />
+
+        <PlateCard
+          name="Suchi 2"
+          bgColor="black"
+          image={food3}
+          alt=""
+          datePosted="04/05/2021"
+          rating={3}
+        />
+
+        <PlateCard
+          name="Suchi 2"
+          bgColor="black"
+          image={food3}
+          alt=""
+          datePosted="04/05/2021"
+          rating={3}
         />
       </div>
     </Container>
